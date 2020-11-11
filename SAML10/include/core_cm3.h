65d251dc1b41cdce6b4d4f31c3e8f0ddb81898ec
Frank Duignan <frank.duignan@gmail.com>
2020-11-11 20:15:08 +0000

    Blinky working!

---
+++ SAML10/include/core_cm3.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+#define __IO volatile
+#define __IM volatile
+#define __OM volatile
+#define __IOM volatile
+#define __I volatile
+#define __O volatile
---
+++ SAML10/jlink.sh
@@ -0,0 +1,1 @@
+JLinkGDBServer -device ATSAML10E16A -speed 1000 -interface SWD -if SWD
